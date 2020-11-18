1d42c5a336091ab38a744f321b8514a73289a3eb
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-18 04:13:50 +0000

    Add Tweeter feed section and style it. Refactor CSS code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  console.log($('#tweet-feed article'))
+  $('#tweet-feed article').hover(function() {
+    $(this).css('box-shadow', '10px 10px 0 0 lightsteelblue')
+    $(this).children('header').children('div.author-account').css('color', 'lightsteelblue')
+  },function() {
+    $(this).css('box-shadow', '')
+    $(this).children('header').children('div.author-account').css('color', '#f4f1ec')
+  })
+})
