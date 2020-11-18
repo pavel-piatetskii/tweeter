b5a12308bad800a379cb44597f4a695e500c1866
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-18 05:21:54 +0000

    Fix the nav bar to the top position. Clear debug code in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,6 @@
  */
 
 $(document).ready(() => {
-  console.log($('#tweet-feed article'))
   $('#tweet-feed article').hover(function() {
     $(this).css('box-shadow', '10px 10px 0 0 lightsteelblue')
     $(this).children('header').children('div.author-account').css('color', 'lightsteelblue')
